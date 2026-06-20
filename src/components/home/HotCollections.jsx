239b295e8e905d9dc2c95e73de18ff41c95ad5fb
superbsparo@gmail.com <superbsparo@gmail.com>
2026-06-20 00:30:15 -0400

    added loading variable, still need to work on getting loading nft's to show

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -11,12 +11,35 @@ import "../../css/Slider.css";
 
 const HotCollections = () => {
 	const [userData, setUserData] = useState([]);
+	const [loading, setLoading] = useState(true);
 	var settings = {
 		dots: false,
 		infinite: true,
 		speed: 500,
 		slidesToShow: 4,
 		slidesToScroll: 1,
+
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					dots: true,
+					slidesToShow: 1,
+				},
+			},
+		],
 	};
 
 	async function fetchData() {
@@ -24,16 +47,17 @@ const HotCollections = () => {
 			const response = await axios.get(
 				"https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections",
 			);
-
 			setUserData(response.data);
 			console.log(userData);
 		} catch (error) {
 			console.log(error);
 		}
 	}
+
 	useEffect(() => {
 		fetchData();
 	}, []);
+
 	return (
 		<section id="section-collections" className="no-bottom">
 			<div className="container">
@@ -45,37 +69,43 @@ const HotCollections = () => {
 						</div>
 					</div>
 					<Slider {...settings}>
-						{userData.map((item, index) => (
-							<div className="nft" key={index}>
-								<div className="nft_coll">
-									<div className="nft_wrap">
-										<Link to="/item-details">
-											<img
-												src={item.nftImage}
-												className="lazy img-fluid"
-												alt=""
-											/>
-										</Link>
+						{!loading
+							? userData.map((item, index) => (
+									<div className="nft" key={index}>
+										<div className="nft_coll">
+											<div className="nft_wrap">
+												<Link to="/item-details">
+													<img
+														src={item.nftImage}
+														className="lazy img-fluid"
+														alt=""
+													/>
+												</Link>
+											</div>
+											<div className="nft_coll_pp">
+												<Link to="/author">
+													<img
+														className="lazy pp-coll"
+														src={item.authorImage}
+														alt=""
+													/>
+												</Link>
+												<i className="fa fa-check"></i>
+											</div>
+											<div className="nft_coll_info">
+												<Link to="/explore">
+													<h4>{item.title}</h4>
+												</Link>
+												<span>ERC-{item.code}</span>
+											</div>
+										</div>
 									</div>
-									<div className="nft_coll_pp">
-										<Link to="/author">
-											<img
-												className="lazy pp-coll"
-												src={item.authorImage}
-												alt=""
-											/>
-										</Link>
-										<i className="fa fa-check"></i>
+								))
+							: new Array(4).fill(0).map((_, index) => (
+									<div className="nft slick-loading loading_nft" key={index}>
+										<div className="nft_coll"></div>
 									</div>
-									<div className="nft_coll_info">
-										<Link to="/explore">
-											<h4>{item.title}</h4>
-										</Link>
-										<span>ERC-{item.code}</span>
-									</div>
-								</div>
-							</div>
-						))}
+								))}
 					</Slider>
 				</div>
 			</div>
