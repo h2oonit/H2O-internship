179ba5ddad943939cda78a6e67e071dff8bcea9b
superbsparo@gmail.com <superbsparo@gmail.com>
2026-07-02 00:32:09 -0400

    finished explore section, currently working on item details skeleton state

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -8,8 +8,8 @@ import { Countdown } from "../UI/Countdown";
 
 const ExploreItems = () => {
 	const [userData, setUserData] = useState([]);
+	const [loadMore, setLoadMore] = useState(8);
 	const [loading, setLoading] = useState(true);
-  const [loadMore, setLoadMore] = useState(8);
 
 	async function fetchData() {
 		try {
@@ -30,7 +30,6 @@ const ExploreItems = () => {
 			const response = await axios.get(
 				"https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=likes_high_to_low",
 			);
-			console.log(response.data);
 			setUserData(response.data);
 		} catch (error) {
 			console.error("Error fetching explore data:", error);
@@ -41,7 +40,7 @@ const ExploreItems = () => {
 
 	useEffect(() => {
 		fetchData();
-    fetchFilteredData();
+		fetchFilteredData();
 	}, []);
 
 	return (
@@ -54,69 +53,102 @@ const ExploreItems = () => {
 					<option value="likes_high_to_low">Most liked</option>
 				</select>
 			</div>
-			{userData.slice(0, loadMore).map((user, index) => (
-				<div
-					key={index}
-					className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-					style={{ display: "block", backgroundSize: "cover" }}
-				>
-					<div className="nft__item">
-						<div className="author_list_pp">
-							<Link
-								to={`/author/${user.authorId}`}
-								data-bs-toggle="tooltip"
-								data-bs-placement="top"
-							>
-								<img className="lazy" src={user.authorImage} alt="" />
-								<i className="fa fa-check"></i>
-							</Link>
-						</div>
-						{!loading && user.expiryDate ? (
-							<Countdown expiryTime={user.expiryDate} />
-						) : (
-							<div className="de_countdown-empty"></div>
-						)}
+			{!loading
+				? userData.slice(0, loadMore).map((user, index) => (
+						<div
+							key={index}
+							className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+							style={{ display: "block", backgroundSize: "cover" }}
+						>
+							<div className="nft__item">
+								<div className="author_list_pp">
+									<Link
+										to={`/author/${user.authorId}`}
+										data-bs-toggle="tooltip"
+										data-bs-placement="top"
+									>
+										<img className="lazy" src={user.authorImage} alt="" />
+										<i className="fa fa-check"></i>
+									</Link>
+								</div>
+								{!loading && user.expiryDate ? (
+									<Countdown expiryTime={user.expiryDate} />
+								) : (
+									<div className="de_countdown-empty"></div>
+								)}
 
-						<div className="nft__item_wrap">
-							<div className="nft__item_extra">
-								<div className="nft__item_buttons">
-									<button>Buy Now</button>
-									<div className="nft__item_share">
-										<h4>Share</h4>
-										<a href="" target="_blank" rel="noreferrer">
-											<i className="fa fa-facebook fa-lg"></i>
-										</a>
-										<a href="" target="_blank" rel="noreferrer">
-											<i className="fa fa-twitter fa-lg"></i>
-										</a>
-										<a href="">
-											<i className="fa fa-envelope fa-lg"></i>
-										</a>
+								<div className="nft__item_wrap">
+									<div className="nft__item_extra">
+										<div className="nft__item_buttons">
+											<button>Buy Now</button>
+											<div className="nft__item_share">
+												<h4>Share</h4>
+												<a href="" target="_blank" rel="noreferrer">
+													<i className="fa fa-facebook fa-lg"></i>
+												</a>
+												<a href="" target="_blank" rel="noreferrer">
+													<i className="fa fa-twitter fa-lg"></i>
+												</a>
+												<a href="">
+													<i className="fa fa-envelope fa-lg"></i>
+												</a>
+											</div>
+										</div>
+									</div>
+									<Link to={`/item-details/${user.nftId}`}>
+										<img
+											src={user.nftImage}
+											className="lazy nft__item_preview"
+											alt=""
+										/>
+									</Link>
+								</div>
+								<div className="nft__item_info">
+									<Link to={`/item-details/${user.nftId}`}>
+										<h4>{user.nftName}</h4>
+									</Link>
+									<div className="nft__item_price">{user.price} ETH</div>
+									<div className="nft__item_like">
+										<i className="fa fa-heart"></i>
+										<span>{user.likes}</span>
 									</div>
 								</div>
 							</div>
-							<Link to="/item-details">
-								<img src={user.nftImage} className="lazy nft__item_preview" alt="" />
-							</Link>
 						</div>
-						<div className="nft__item_info">
-							<Link to="/item-details">
-								<h4>{user.nftName}</h4>
-							</Link>
-							<div className="nft__item_price">{user.price} ETH</div>
-							<div className="nft__item_like">
-								<i className="fa fa-heart"></i>
-								<span>{user.likes}</span>
+					))
+				: new Array(8).fill(0).map((_, index) => (
+						<div className="nft skeleton_nft" key={index}>
+							<div className="nft_coll skeleton-card">
+								<div className="nft_wrap">
+									<div className="skeleton skeleton-img"></div>
+								</div>
+								<div className="nft_coll_pp">
+									<div className="skeleton skeleton-avatar"></div>
+								</div>
+								<div className="nft_coll_info">
+									<div className="skeleton skeleton-title"></div>
+									<div className="skeleton skeleton-subtitle"></div>
+								</div>
 							</div>
 						</div>
-					</div>
+					))}
+
+			{!loading ? (
+				<div className="col-md-12 text-center">
+					<Link
+						to=""
+						id="loadmore"
+						onClick={() => setLoadMore(loadMore + 8)}
+						className="btn-main lead"
+					>
+						Load more
+					</Link>
 				</div>
-			))}
-			<div className="col-md-12 text-center">
-				<Link to="" id="loadmore" onClick={() => setLoadMore(loadMore + 8)} className="btn-main lead">
-					Load more
-				</Link>
-			</div>
+			) :
+        <div className="col-md-12 text-center">
+          <div className="skeleton-box load-more-btn"></div>
+        </div>
+      }
 		</>
 	);
 };
